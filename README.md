c456e49e3a4db2f1a50f71aa39fd7d4969fafa94
Tom Clarkson <tom@orangeguava.com>
2014-03-31 15:09:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# GitCheck
+
+A tool for checking the status of multiple git repositories. It will report the status of any repositories under the current working directory with uncommitted changes.
+
+## Installation
+
+    npm install -g gitcheck
+
+## Usage
+
+    gitcheck
+
+Returns
+
+    2 changed files in c:\git\devtools\gitcheck
+    5 changed files in c:\git\devtools\gitrunner
---
+++ gitcheck.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var fs = require("fs");
+var path = require("path");
+
+var gitrunner = require("gitrunner");
+
+var check = function(folder, onupdate, oncompleted) {
+    gitrunner.fullStatus(folder, function(status) {
+        if (status.isRepo) {
+            onupdate(status),
+            oncompleted();
+        } else {
+            // Not a git repo - check subfolders
+            fs.readdir(folder, function(err, files) {
+                (function loopfn(i, completed) {
+                    if (!(i < files.length)) {
+                        if (completed) completed();
+                        return;
+                    }
+                    var fp = path.resolve(folder, files[i]);
+                    fs.stat(fp, function(err, stat) {
+                        if (stat.isDirectory()) {
+                            check(fp, onupdate, function() {
+                                loopfn(i + 1, completed);
+                            });
+                        } else {
+                            loopfn(i + 1, completed);
+                        }
+                    });
+                })(0, oncompleted)
+            });
+
+        }
+    })
+};
+
+
+var knownfolders = {};
+
+
+check(process.cwd(), function(folder) {
+        if (folder.changedFiles.length > 0) {
+            console.log(folder.changedFiles.length + " changed files in " + folder.path);
+            //console.log(folder)
+        }
+    },
+    function() {
+        console.log("completed check");
+        // todo: fire update for any missing folders in knownfolders
+    })
